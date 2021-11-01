a14622471c54876d4d551c13d8645bc887455bc1
Nguyen Dang Minh <nguyendangminh@gmail.com>
2021-11-01 17:15:57 +0700

    Pass sleep time via URL parameter

--- main.go
+++ main.go
@@ -1,24 +1,34 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 )
 
-var sleepTime int
-
 func main() {
-	flag.IntVar(&sleepTime, "s", 0, "sleep time (default 0 second)")
-	flag.Parse()
-
 	fmt.Println("oldman is ready to Go!")
 	http.HandleFunc("/", hello)
 	http.ListenAndServe(":1203", nil)
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	values, _ := query["t"]
+
+	var sleepTime int = 0
+	if len(values) > 0 {
+		i, err := strconv.Atoi(values[0])
+		if err != nil {
+			// handle error
+			fmt.Println(err)
+			http.Error(w, err.Error(), 400)
+			return
+		}
+		sleepTime = i
+	}
+
 	time.Sleep(time.Duration(sleepTime) * time.Second)
 	fmt.Fprintf(w, fmt.Sprintf("hello, oldman is ready to Go! sleep %d seconds", sleepTime))
 }
