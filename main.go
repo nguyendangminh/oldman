dcb8f973f40b8a59da2f07d57f845b60955d98ea
Nguyen Dang Minh <nguyendangminh@gmail.com>
2021-11-01 17:42:17 +0700

    Run multiple HTTP servers

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 	"time"
@@ -9,8 +10,28 @@ import (
 
 func main() {
 	fmt.Println("oldman is ready to Go!")
-	http.HandleFunc("/", hello)
+
+	// HTTP2 server
+	srv := &http.Server{Addr: ":1204", Handler: http.HandlerFunc(http2Hello)}
+	fmt.Println("HTTP2 server is serving on https://x.x.x.x:1204")
+	go func() {
+		log.Fatal(srv.ListenAndServeTLS("key/server.crt", "key/server.key"))
+	}()
+
+	// HTTP1.1 server
+	http.HandleFunc("/", http1Hello)
+	fmt.Println("HTTP1.1 server is serving on http://x.x.x.x:1203")
 	http.ListenAndServe(":1203", nil)
+}
+
+func http1Hello(w http.ResponseWriter, r *http.Request) {
+	log.Printf("HTTP1 server: %s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+	hello(w, r)
+}
+
+func http2Hello(w http.ResponseWriter, r *http.Request) {
+	log.Printf("HTTP2 server: %s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+	hello(w, r)
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
